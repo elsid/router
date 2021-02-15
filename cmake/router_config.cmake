9aee0d0b7a914008a4f7720e2ae38ecadddcc50a
Roman Siromakha <elsid.mail@gmail.com>
2021-02-15 22:21:17 +0100

    Add router header library

--- cmake/router_config.cmake
+++ cmake/router_config.cmake
@@ -1,3 +1,7 @@
 include(CMakeFindDependencyMacro)
 
+find_dependency(expected)
+find_dependency(tl::expected)
+find_dependency(tl-expected)
+
 include("${CMAKE_CURRENT_LIST_DIR}/router_targets.cmake")
